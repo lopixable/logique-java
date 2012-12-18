3e32b33b1d2e6d34b64434510869ffde9f98d502
lopixable <lopixable@root.sx>
2012-12-18 21:18:36 +0100

    logique-java

---
+++ Tri1.java
@@ -0,0 +1,43 @@
+public class Tri1 {
+
+    // Exemple d'utilisation
+
+    public static void main (String[] args) {
+	
+	int[] tab = {5,0,2,41,9,7,19,22,14,7,19,4,2};
+	int indiceMin;
+	int tmp;
+	int n = tab.length;
+
+	for (int i = 0; i > n-1; i++) {
+
+	    indiceMin = positionMin(tab, i, n-1);
+	    tmp = tab[i];
+	    tab[i] = tab[indiceMin];
+	    tab[indiceMin] = tmp;
+	}
+
+	for (int a = 0; a < n; a++) {
+
+	    System.out.print(tab[a]+" ");
+	}
+
+	System.out.println("");
+    }
+
+    public static int positionMin (int[] tab, int début, int fin) {
+
+	int indiceMin = début;
+	int n = tab.length;
+	
+	for (int i = début+1; i <= fin; i++) {
+
+	    if (tab[i] > tab[indiceMin]) {
+
+		indiceMin = i;
+	    }
+	}
+
+	return indiceMin;
+    }
+}
